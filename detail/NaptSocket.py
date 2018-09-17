8a14805226424e91a92b92d0d6d44c519f22c418
Kouichirou Okada <kouichirou_okada@rainforest.tokyo>
2018-09-17 11:29:13 +0000

    タイムアウト設定変更

--- detail/NaptSocket.py
+++ detail/NaptSocket.py
@@ -137,7 +137,7 @@ class NaptSocket(object):
                 data= self.send_buffers.popleft()   # Dequeue
 
             try:
-                #self.socket.settimeout(5.0)
+                self.socket.settimeout(5.0)
                 self.socket.sendall(data)
             except Exception as ex:
                 Utils.print_exception(ex)
